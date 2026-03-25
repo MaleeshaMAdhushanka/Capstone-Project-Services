3e542450ae8e55d76527569bf32cc1748e1e6240
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2026-03-26 01:12:40 +0530

    Add ecosystem configuration for student, program, and enrollment services

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: "student-service",
+      script: "java -jar student-service/target/Student-Service-1.0.0.jar",
+      log_file: "./logs/student-service.log",
+      instance: 2
+    },
+
+
+    {
+      name: "program-service",
+      script: "java -jar program-service/target/Program-Service-1.0.0.jar ",
+      log_file: "./logs/program-service.log",
+      instance: 2
+    },
+    {
+      name: "enrollment-service",
+      script: "java -jar enrollment-service/target/Enrollment-Service-1.0.0.jar ",
+      log_file: "./logs/enrollment-service.log",
+      instance: 2
+    }
+  ]
+}
